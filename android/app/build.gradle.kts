4ea150cd86581a479f9e2e73cfd293eb1f5e0e07
Huoth Sitha <Huothsitha@gmail.com>
2026-06-15 14:22:10 +0700

    Fix signing config for CI

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,13 +37,15 @@ android {
     }
 
     signingConfigs {
-        create("release") {
+    create("release") {
+        if (keystorePropertiesFile.exists()) {
             keyAlias = keystoreProperties["keyAlias"] as String
             keyPassword = keystoreProperties["keyPassword"] as String
             storeFile = file(keystoreProperties["storeFile"] as String)
             storePassword = keystoreProperties["storePassword"] as String
         }
     }
+}
 
     buildTypes {
         release {
